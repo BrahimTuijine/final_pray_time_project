7fa7653209d16c58e5b331ae7444dedffb77ce9f
Brahim Tuijine <brahimtuijine220@gmail.com>
2022-06-25 17:16:05 +0100

    init

---
+++ android/app/src/main/kotlin/com/example/final_pray_time_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.final_pray_time_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() {
+  //disable Landscape mode
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
+}
